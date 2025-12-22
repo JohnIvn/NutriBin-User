c9064b964b0a1e7fff21ba75ee8d36e60f52005c
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-12-22 19:32:05 +0800

    Layout configs

--- web/Frontend/src/App.jsx
+++ web/Frontend/src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MainLayout from "@/layouts/MainLayout";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-
-    </>
+    <MainLayout></MainLayout>
   )
 }
-
-export default App
