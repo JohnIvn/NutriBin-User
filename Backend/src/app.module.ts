7d21c5fc4afb4907a4c307ea69fe6b101e9e541a
イアン <janivantot@gmail.com>
2026-02-16 23:03:15 +0800

    Hardware Controller Modified
    Supports Only Mobile

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -27,7 +27,7 @@ import { SupportService } from './service/support/support.service';
 import { MachineNotificationsModule } from './service/notification/machine-notifications.module';
 import { DataScienceController } from './controllers/stats/data-science.controller';
 import { SupportController } from './controllers/user/support.controller';
-import { HardwareController } from './controllers/hardware/hardware.controller';
+import { HardwareController } from './controllers/mobile/hardware.controller';
 
 @Module({
   imports: [
